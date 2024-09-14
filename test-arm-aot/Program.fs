18bcde17e14f0ef78cb45af57900c42872a73dd5
Oleksiy Slyshyk <alexslyshyk@gmail.com>
2024-09-14 10:52:09 +0300

    Update ARM runtime in build command

--- test-arm-aot/Program.fs
+++ test-arm-aot/Program.fs
@@ -1,4 +1,4 @@
-﻿//dotnet publish .\test-arm-aot\test-arm-aot.fsproj -c Release -f net9.0 --tl:off --verbosity n --sc --runtime linux-arm  /property:PublishTrimmed=True /property:IncludeNativeLibrariesForSelfExtract=True /property:DebugType=None /property:DebugSymbols=False /property:PublishAot=True /p:StripSymbols=True
+﻿//dotnet publish .\test-arm-aot\test-arm-aot.fsproj -c Release -f net9.0 --tl:off --verbosity n --sc --runtime linux-musl-arm  /property:PublishTrimmed=True /property:IncludeNativeLibrariesForSelfExtract=True /property:DebugType=None /property:DebugSymbols=False /property:PublishAot=True /p:StripSymbols=True
 //dotnet publish .\test-arm-aot\test-arm-aot.fsproj -c Release -f net9.0 --tl:off --verbosity n --sc --runtime linux-arm  /property:PublishTrimmed=True /property:IncludeNativeLibrariesForSelfExtract=True /property:DebugType=None /property:DebugSymbols=False /property:PublishAot=False /property:PublishSingleFile=True /p:StripSymbols=True
 open System
 open FSharpPlus
